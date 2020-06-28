a1e5690e4fad6d05799b5264e7958f3a97c20040
Qusic <qusicx@gmail.com>
2020-06-28 04:57:52 +0000

    display unauthorized message in login page

--- login.go
+++ login.go
@@ -3,35 +3,61 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handleLogin(w http.ResponseWriter, r *http.Request, ctx *context) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, loginPage, config.botName, config.pathPrefix, ctx.app)
+	token := tokenFromCookie(r)
+	valid, user := useToken(token, time.Now())
+	if !valid {
+		fmt.Fprint(w, loginPageStart)
+		fmt.Fprintf(w, unauthenticatedBody, config.botName, config.pathPrefix, ctx.app)
+		fmt.Fprint(w, loginPageEnd)
+		return
+	}
+	access, ok := config.appAccess[ctx.app][user]
+	if !ok || !access {
+		fmt.Fprint(w, loginPageStart)
+		fmt.Fprintf(w, unauthorizedBody, user)
+		fmt.Fprint(w, loginPageEnd)
+		return
+	}
+	http.Redirect(w, r, config.appURL[ctx.app], http.StatusFound)
 }
 
-const loginPage = `
+const loginPageStart = `
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<style>
-			body {
-				width: 100vw;
-				height: 100vh;
-				margin: 0;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-			}
-		</style>
-	</head>
-	<body>
-		<script async src="https://telegram.org/js/telegram-widget.js"
-			data-telegram-login="%v"
-			data-size="large"
-			data-auth-url="%v/%v/callback">
-		</script>
-	</body>
+<head>
+<meta charset="utf-8">
+<style>
+body {
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+	font-family: system-ui;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
+</head>
+<body>
+`
+
+const loginPageEnd = `
+</body>
 </html>
+`
+
+const unauthenticatedBody = `
+<script async src="https://telegram.org/js/telegram-widget.js"
+	data-telegram-login="%v"
+	data-size="large"
+	data-auth-url="%v/%v/callback">
+</script>
+`
+
+const unauthorizedBody = `
+You have logged in as @%v, but are not authorized to access this resource.
 `
--- login_test.go
+++ login_test.go
@@ -5,26 +5,58 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHandleLogin(t *testing.T) {
+	config.botName = "test_bot"
+	config.pathPrefix = "test_prefix"
+	config.cookieName = "cookie"
+	app := "test_app"
+	url := "/test_app_url"
+	_,
+		validUser, invalidUser,
+		validToken1, validToken2,
+		invalidTokenNoSignature, invalidTokenBadSignature := setupTestToken()
+	config.appURL = map[string]string{app: url}
 	for _, data := range []struct {
-		botName, pathPrefix, app string
+		access string
+		token  string
+		valid  int
 	}{
-		{botName: "bot", pathPrefix: "/", app: "app"},
-		{botName: "test_bot", pathPrefix: "/auth", app: "test_app"},
+		{access: validUser, token: validToken1, valid: 1},
+		{access: validUser, token: validToken2, valid: 1},
+		{access: validUser, token: invalidTokenNoSignature, valid: -1},
+		{access: validUser, token: invalidTokenBadSignature, valid: -1},
+		{access: invalidUser, token: validToken1, valid: 0},
+		{access: invalidUser, token: validToken2, valid: 0},
+		{access: invalidUser, token: invalidTokenNoSignature, valid: -1},
+		{access: invalidUser, token: invalidTokenBadSignature, valid: -1},
 	} {
-		config.botName = data.botName
-		config.pathPrefix = data.pathPrefix
-		ctx := context{app: data.app}
+		config.appAccess = map[string]map[string]bool{app: {data.access: true}}
+		state.authCache = map[string]time.Time{}
+		ctx := context{app: app}
 		rr := httptest.NewRecorder()
+		cookie := http.Cookie{Name: config.cookieName, Value: data.token}
 		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.AddCookie(&cookie)
 		handleLogin(rr, r, &ctx)
-		assert.Equal(t, http.StatusOK, rr.Code)
-		body := rr.Body.String()
-		assert.Contains(t, body, fmt.Sprintf("data-telegram-login=\"%v\"", data.botName))
-		assert.Contains(t, body, fmt.Sprintf("data-auth-url=\"%v/%v/callback\"", data.pathPrefix, data.app))
+		switch data.valid {
+		case 1:
+			assert.Equal(t, http.StatusFound, rr.Code)
+			location, _ := rr.Result().Location()
+			assert.Equal(t, url, location.String())
+		case 0:
+			assert.Equal(t, http.StatusOK, rr.Code)
+			body := rr.Body.String()
+			assert.Contains(t, body, fmt.Sprintf("@%v", validUser))
+		case -1:
+			assert.Equal(t, http.StatusOK, rr.Code)
+			body := rr.Body.String()
+			assert.Contains(t, body, fmt.Sprintf("data-telegram-login=\"%v\"", config.botName))
+			assert.Contains(t, body, fmt.Sprintf("data-auth-url=\"%v/%v/callback\"", config.pathPrefix, app))
+		}
 	}
 }
